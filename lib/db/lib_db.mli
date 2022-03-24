c5934543c94b18f0a62c315853b5a3ac62d3a24e
gr-im <grimfw@gmail.com>
2022-03-24 07:58:11 +0100

    Add conveinent type aliases for Caqti
    This change may seem trivial, but it simplifies the model signatures
    from `a -> b -> Caqti_lwt.connection -> c` to `a -> b -> Lib_db.t -> c`.

--- lib/db/lib_db.mli
+++ lib/db/lib_db.mli
@@ -1,5 +1,9 @@
 open Lib_common
 
+type t = Caqti_lwt.connection
+
+module type T = Caqti_lwt.CONNECTION
+
 val try_ : ('a, Caqti_error.t) result Lwt.t -> 'a Try.t Lwt.t
 val make_uri : Env.t -> Uri.t
 
