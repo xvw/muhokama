7403854a7649313af87adb40950778d1b73e3b46
Hakimba <hakim.baaloudj@gmail.com>
2022-06-01 12:11:59 +0200

    add mli stuff

--- app/views/category_views.mli
+++ app/views/category_views.mli
@@ -8,6 +8,8 @@ module Category :
       Models.Category.t -> [> Html_types.tr ] Tyxml_html.elt
     val all : Models.Category.t list -> [> Html_types.table ] Tyxml_html.elt
   end
+
+(** a view for listing all categories **)
 val categories :
   ?flash_info:Models.Flash_info.t ->
   ?user:Models.User.t ->
