724389fbddb16376b838e3749d417f6cbf917398
Hakimba <hakim.baaloudj@gmail.com>
2022-07-21 01:58:43 +0200

    hide functions which are not use outside of category_view

--- app/views/category_views.mli
+++ app/views/category_views.mli
@@ -1,10 +1,4 @@
-val category_name_input : [> Html_types.div ] Tyxml_html.elt
-val category_description_input : [> Html_types.div ] Tyxml_html.elt
-val submit_button : [> Html_types.div ] Tyxml_html.elt
 val creation_form : string -> [> Html_types.form ] Tyxml_html.elt
-val category_line : Models.Category.t -> [> Html_types.tr ] Tyxml_html.elt
-val category_topics_count_line :
-  string * string * int -> [> Html_types.tr ] Tyxml_html.elt
 val all : Models.Category.t list -> [> Html_types.table ] Tyxml_html.elt
 
 (** a view for listing all categories **)
